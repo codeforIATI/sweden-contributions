78e23373f2ff1a2a830c7e2be4d3a202e5675033
Mark Brough <mark.brough@gmail.com>
2022-09-05 13:06:31 +0000

    Output index files

--- run.py
+++ run.py
@@ -21,13 +21,15 @@ for iati_identifier in iati_identifiers:
         continue
     _contrib, _country, _sector = matches.groups()
     _corrected_contrib = _contrib.split("A")
-    if _corrected_contrib:
+    if _corrected_contrib is not None:
         _contrib = _corrected_contrib[0]
     if _country not in contributions.keys():
         contributions[_country] = {}
     if _contrib not in contributions[_country].keys():
         contributions[_country][_contrib] = []
     contributions[_country][_contrib].append(iati_identifier)
+
+index_countries = {}
 
 for country_code, country_data in contributions.items():
     iso2_country_code = iso3_iso2.get(country_code)
@@ -41,3 +43,10 @@ for country_code, country_data in contributions.items():
     for contribution_code, iati_identifiers in country_data.items():
         with open('output/{}/{}.json'.format(iso2_country_code, contribution_code), 'w') as outfile:
             json.dump(iati_identifiers, outfile)
+    with open('output/{}/index.json'.format(iso2_country_code), 'w') as out_country_index:
+        json.dump([f"{contribution_code}.json" for countribution_code in country_data.keys()], out_country_index)
+
+    index_countries[iso2_country_code] = f"{iso2_country_code}/index.json"
+
+with open('output/index.json', 'w') as index_file:
+    json.dump(index_countries, index_file)
